4f61170c4153caeebdfcf8397470da72d8ae60b8
Loktev Roman <loktevra@gmail.com>
2018-11-08 07:33:38 +0500

    немного отрефакторен сервер на маршруты

--- finance-crowler/src/httpServerStart.js
+++ finance-crowler/src/httpServerStart.js
@@ -5,6 +5,41 @@ const url = require('url');
 
 const dbo = require('./dbo');
 
+async function productsInfo(req, res) {
+  const products = await dbo.alfaCapital.getProductsList();
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify({ status: 'success', data: products }));
+  res.end();
+}
+
+async function graph(req, res) {
+  try {
+    const {
+      query,
+    } = url.parse(req.url, true);
+
+    const graph = await dbo.alfaCapital.getGraph(query.id);
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ status: 'success', data: graph }));
+    res.end();
+  } catch (error) {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ status: 'fail', errors: [
+      {
+        code: 400,
+        description: String(error),
+      }
+    ] }));
+    res.end();
+  }
+}
+
+const routesGet = new Map([
+  ['/alfa-capital/products-info', productsInfo],
+  ['/alfa-capital/graph', graph]
+])
+
 
 let server;
 
@@ -15,32 +50,11 @@ module.exports = () => new Promise(function httpServerStart(resolve, reject) {
       cert: fs.readFileSync(`${process.env.HOME}/data/cert.pem`)
     }
     server = http2.createSecureServer(options,async (req, res) => {
-      console.log(`Запрос: ${req.url}`)
       const {
         pathname,
-        query,
       } = url.parse(req.url, true)
-      if (pathname === '/alfa-capital/products-info') {
-        const products = await dbo.alfaCapital.getProductsList();
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify({ status: 'success', data: products }));
-        res.end();
-      } else if (pathname === '/alfa-capital/graph') {
-        try {
-          const graph = await dbo.alfaCapital.getGraph(query.id);
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.write(JSON.stringify({ status: 'success', data: graph }));
-          res.end();
-        } catch (error) {
-          res.writeHead(400, { 'Content-Type': 'application/json' });
-          res.write(JSON.stringify({ status: 'fail', errors: [
-            {
-              code: 400,
-              description: String(error),
-            }
-          ] }));
-          res.end();
-        }
+      if (routesGet.has(pathname)) {
+        routesGet.get(pathname)(req, res);
       } else {
         res.writeHead(404, { 'Content-Type': 'application/json' });
         res.write(JSON.stringify({ status: 'fail', errors: [
