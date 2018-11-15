1ac7bc890cf80ab4199b35d5243ab52ced139aa6
Loktev Roman <loktevra@gmail.com>
2018-11-15 09:22:16 +0500

    добавлено деление конфига на дев и прод и чанки

--- webpack-lesson/webpack.config.js
+++ webpack-lesson/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-module.exports = {
+const baseConfig = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -27,8 +28,7 @@ module.exports = {
       },
       {
         test: /\.less$/,
-        use: [ 
-          MiniCssExtractPlugin.loader, 
+        use: [
           'css-loader', 
           'less-loader'
         ],
@@ -36,10 +36,59 @@ module.exports = {
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin(),
     new HtmlWebPackPlugin({
       template: "./index.html",
       filename: "index.html"
     })
   ]
+}
+
+const prodConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+}
+
+const devConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [ 
+          'style-loader',
+        ]
+      }
+    ]
+  }
+}
+
+const smartMerge = merge.smartStrategy({ 'module.rules.use': 'prepend'})
+
+module.exports = (env, { mode }) => {
+  if (mode == 'production') {
+    return smartMerge(baseConfig, prodConfig)
+  }
+
+  return smartMerge(baseConfig, devConfig)
 };
