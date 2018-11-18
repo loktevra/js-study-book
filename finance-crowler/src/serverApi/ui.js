7b82dd458a35fe3e77d77a5cca6f62efa4f0567e
Loktev Roman <loktevra@gmail.com>
2018-11-18 18:08:57 +0500

    ui удалён из сервиса для апи

--- finance-crowler/src/serverApi/ui.js
+++ finance-crowler/src/serverApi/ui.js
@@ -1,13 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-
 const httpServer = require('../services/httpServer');
 
 async function ui(req, res) {
   try {
-    const html = fs.readFileSync(path.resolve(__dirname, '../ui/index.html'), 'UTF-8');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(html);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ status: 'success' }));
+    res.end();
   } catch(e) {
     res.writeHead(400, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify({ status: 'fail', errors: [
