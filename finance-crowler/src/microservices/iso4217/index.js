f6a339ed205276345f8a323afaabe655d6d97eab
Roman Loktev <roman.loktev@antidasoftware.com>
2018-12-29 16:38:20 +0500

    удалён монолит finance-crowler

--- finance-crowler/src/microservices/iso4217/index.js
+++
@@ -1,8 +0,0 @@
-function iso4217() {
-  this.add({role: 'iso-4217', cmd: 'getCurrencyList'}, function (msg, done) {
-    const list = ['foo', 'bar']
-    done(null, {list})
-  })
-}
-
-export default iso4217
